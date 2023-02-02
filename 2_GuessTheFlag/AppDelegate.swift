b415bda0b56142b275d95fedc1d1cbe4202ea580
Brennan Dumm <bjdumm23@gmail.com>
2023-02-02 10:24:04 -0500

    SSH Test

--- 2_GuessTheFlag/AppDelegate.swift
+++ 2_GuessTheFlag/AppDelegate.swift
@@ -12,7 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 //comment
     //Test Push
-
+//SSH Test
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
