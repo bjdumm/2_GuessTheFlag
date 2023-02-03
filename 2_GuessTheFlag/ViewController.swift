effb95e6c40deda69a9a58a7aa1e88ef95bdc621
DoctorSpirit103 <doctorspirit103@gmail.com>
2023-02-03 10:12:07 -0500

    test

--- 2_GuessTheFlag/ViewController.swift
+++ 2_GuessTheFlag/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
- var str = "Swift"
+ var str = "SwiftUI"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
