588ec3daddfac54377b77bcf307c1dbac297f9e4
Brennan Dumm <bjdumm23@gmail.com>
2023-02-03 10:08:57 -0500

    Test

--- 2_GuessTheFlag/ViewController.swift
+++ 2_GuessTheFlag/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     var name = ""
+    var a = 1
+    var b = 2
+    var c = 3
+    var d = 4
     
     override func viewDidLoad() {
         super.viewDidLoad()
